80cbbcdfbd1e99deeaa82a8e2976f8885046abed
Michał Lipowski <lipek92@gmail.com>
2015-05-30 21:45:09 +0200

    routing i proste wyswietlanie

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,7 +8,7 @@
  *
  * Main module of the application.
  */
-angular
+var app = angular
   .module('angularApp', [
     'ngAnimate',
     'ngCookies',
@@ -23,11 +23,63 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
+      .when('/beers', {
+        templateUrl: 'views/beers.html',
+        controller: 'BeersCtrl'
       })
       .otherwise({
         redirectTo: '/'
       });
   });
+
+app.service('beerService', function()
+{
+  var getBeers = function(){
+    return beers;
+  }
+
+  return {
+    getBeers: getBeers()
+  }
+});
+
+var beers = [{
+  name: 'Johannes',
+  description: "Dobre piwko!",
+  images: [
+    "images/gem-02.gif",
+    "images/gem-05.gif",
+    "images/gem-09.gif"
+  ],
+  reviews: [{
+    stars: 5,
+    body: "Moje ulubioneee!",
+    author: "kaziu",
+    createdOn: 1397490980837
+  }, {
+    stars: 1,
+    body: "Nie smakuje mi",
+    author: "smakosz",
+    createdOn: 1397490980837
+  }]
+}, {
+  name: 'Tyskie',
+  description: "Tyskie piwo.",
+  images: [
+    "images/gem-01.gif",
+    "images/gem-03.gif",
+    "images/gem-04.gif",
+  ],
+  reviews: [{
+    stars: 3,
+    body: "Bardzo smaczne!",
+    author: "waldek",
+    createdOn: 1397490980837
+  }, {
+    stars: 4,
+    body: "Polecam!",
+    author: "rysiu132",
+    createdOn: 1397490980837
+  }]
+}];
+
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,9 @@
  * # MainCtrl
  * Controller of the angularApp
  */
-angular.module('angularApp')
+var app = angular.module('angularApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
   });
+
+
