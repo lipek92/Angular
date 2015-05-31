9f3e0d3dfb06ddcc0d72f3cc755f4b2950b4d7dd
Michał Lipowski <lipek92@gmail.com>
2015-05-31 15:55:44 +0200

    wlasne dyrektywy

--- app/scripts/controllers/beers.js
+++ app/scripts/controllers/beers.js
@@ -4,25 +4,54 @@ app.controller('BeersCtrl', function ($scope, beerService) {
     $scope.beers = beerService.getBeers;
   });
 
-app.controller('BeerMenuController', function() {
-  this.menu = 1;
+app.directive('beerMenu', function()
+{
+  return {
+    restrict: 'E',
+    templateUrl: '../../templates/beer-menu.html',
+    controller: function(){
+      this.menu = 1;
 
-  this.setMenu = function(menu){
-    this.menu = menu;
-  };
+      this.setMenu = function(menu){
+        this.menu = menu;
+      };
 
-  this.isSet = function(menu){
-    return (this.menu === menu);
+      this.isSet = function(menu){
+        return (this.menu === menu);
+      };
+    },
+    controllerAs: "beerMenu"
+  }
+});
+
+app.directive('beerComments', function(){
+  return {
+    restrict: 'E',
+    templateUrl: '../../templates/beer-comments.html',
+    controller: function(){
+      this.comment = {};
+
+      this.addComment = function(beer){
+        this.comment.createdOn = Date.now();
+        beer.comments.push(this.comment);
+        this.comment = {};
+      };
+    },
+    controllerAs: 'commentCtrl'
   };
 });
 
-app.controller('CommentController', function(){
+app.directive('beerDescription', function()
+{
+  return {
+    restrict: 'E',
+    templateUrl: 'templates/beer-description.html'
+  };
+});
 
-  this.comment = {};
-  this.comment.createdOn = Date.now();
-
-  this.addComment = function(beer){
-    beer.comments.push(this.comment);
-    this.comment = {};
+app.directive('lastComments', function(){
+  return {
+    restrict: 'E',
+    templateUrl: 'templates/last-comments.html'
   };
 });
